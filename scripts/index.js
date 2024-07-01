38189a8a5faf0403fd7e561e2a66df4d75432e7a
Mackay Grange <mackay.grange@gmail.com>
2024-06-30 19:30:18 -0600

    first commit

---
+++ scripts/index.js
@@ -0,0 +1,32 @@
+document.addEventListener("DOMContentLoaded", function() 
+{
+    document.getElementById("nav_home_button").addEventListener("click", function() 
+    {
+        window.location.href = "index.html";
+    });
+
+    document.getElementById("nav_play_button").addEventListener("click", function() 
+    {
+        window.location.href = "play.html";
+    });
+
+    document.getElementById("nav_instructions_button").addEventListener("click", function() 
+    {
+        window.location.href = "instructions.html";
+    });
+
+    document.getElementById("main_roll_button").addEventListener("click", function() 
+    {
+        ;
+    });
+
+    document.getElementById("main_pass_button").addEventListener("click", function() 
+    {
+        ;
+    });
+
+    document.getElementById("main_restart_button").addEventListener("click", function() 
+    {
+        ;
+    });
+});
